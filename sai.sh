2c3035d08af7ab3b2d44712570107198cf7ed58e
dinesh93900 <dinesh14701@gmail.com>
2024-06-14 07:25:02 +0000

    first commit

---
+++ sai.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+r=21
+m=21
+q=21
+4=21
+echo "the variable r value is : $r"
+echo "the variable m value is : $m"
+echo "the variable q value is : $q"
+echo "the variable 4 value is : $4"
+
+
+echo "iam dinesh "
+echo "today is shellscript class"
+echo "today date is ..."
+date 
+<<dine
+hello everyone 
+iam dinesh
+iam from andhrapradesh
+welcome to home 
+please support
+dine
+echo $o
+echo $1
+set -x
+echo $2
+echo $3
+echo $4
+set +x
+#my name is dinesh
+#iam from andhrapradesh
+#welcome to home
+expr 2 + 3
+expr 10 - 5
+expr 5 \* 1
+expr 5 / 1
+expr 2 % 2
+echo "function demo starts"
+dinesh function (  )
+dinesh function
+{
+	echo "iam dinesh"
+	echo "call me dinesh"
+	echo "come to work"
+	echo "hello everyone"
+}
+dinesh function
+ 
---
+++ sasi.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+echo "iam dinesh"
+echo "iam from andhhrapradesh"
+echo "i completed my degree in bsc H&HA"
+echo "now iam studing devops cource"
+echo "my age is "
+echo 
+today date is
+echo "date"
+#iam dinesh 
+#call me dinesh
+#going to movie'
+<<dinesh
+come to home 
+go to home
+where are you
+what your doing
+hello everyone
+dinesh
+expr 2 + 2
+expr 6 - 2
+expr 2 \* 2
+expr 2 / 2
